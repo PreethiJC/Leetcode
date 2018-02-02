1c9c077b2292371c1a8c600ae2d590bdd6218562
preethi <preethi.jc92@hotmail.com>
2018-02-02 16:20:36 -0500

    Fifth Commit

---
+++ Easy/RomanToInteger.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        sUpper = s.upper()
+        symbols = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        dig = 0
+        for i, v in enumerate(s):
+
+            if i != len(s) - 1:
+                if symbols[v] < symbols[sUpper[i+1]]:
+                    dig -= symbols[v]
+                else:
+                    dig += symbols[v]
+            else:
+                dig += symbols[v]
+        return dig
+
+s = Solution()
+print(s.romanToInt("MCMLXXXIV"))
+
+
