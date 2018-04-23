6e3d2db5bf52166e548c1cc93f9c1ee006e542ec
preethi <preethi.jc92@hotmail.com>
2018-04-23 16:06:39 -0400

    Hamming Distance
    Stats:
    Testcases -- 149
    Runtime -- 34 ms
    
    Your runtime beats 84.81 % of python submissions.

---
+++ Easy/HammingDistance.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        ans = x ^ y
+        return bin(ans)[2:].count('1')
