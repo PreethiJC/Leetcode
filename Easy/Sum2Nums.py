8e908b7db1d06453aae2fa134bcbd702de9771a3
preethi <preethi.jc92@hotmail.com>
2018-04-25 09:58:12 -0400

    Sum of Two Integers
    Testcases -- 13
    Runtime -- 52 ms
    
    Your runtime beats 1.95 % of python submissions.

---
+++ Easy/Sum2Nums.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        return sum([a, b])
+
+s = Solution()
+print s.getSum(-1, 1)
