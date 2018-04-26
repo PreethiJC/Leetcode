29e6ccc28a7cb4b0ed18f8cd34c4382f67062f0f
preethi <preethi.jc92@hotmail.com>
2018-04-26 11:57:15 -0400

    Min Stack
    Stats:
    Testcases -- 18
    Runtime -- 1120 ms
    
    Your runtime beats 7.46 % of python submissions.

---
+++ Google/MinStack.py
@@ -0,0 +1,32 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return min(self.stack)
