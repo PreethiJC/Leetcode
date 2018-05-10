e7deee322925645633a3d10e60f05dc3f1636b1a
preethi <preethi.jc92@hotmail.com>
2018-05-10 10:34:50 -0400

    Longest Palindromic Substring
    Stats:
    Testcases -- 94
    Runtime -- 5695 ms
    
    Your runtime beats 13.89 % of python submissions.

--- Medium/LongestPalindrome.py
+++ Medium/LongestPalindrome.py
@@ -1,22 +1,47 @@
-def longestPalindrome(word):
-    sub = word[:2]
-    lngth = 0
-    longestSub = ""
-    if sub == sub[::-1]:
-        lngth = len(sub)
-    for letter in word[2:]:
-        sub += letter
-        if sub == sub[::-1]:
-            if lngth < len(sub):
-                lngth = len(sub)
-                longestSub = sub
+class Solution(object):
+    def longestPalindrome(self, word):
+        sub = word[::-1]
+        subStr = self.longestSubstring(word, sub)
+        if subStr == subStr[::-1]:
+            return subStr
         else:
-            sub = sub[1:]
-            if sub == sub[::-1]:
-                if lngth < len(sub):
-                    lngth = len(sub)
-                    longestSub = sub
-    return longestSub
+            return word[0]
 
-print(longestPalindrome("babaddab"))
+    def longestSubstring(self, word, sub):
+        li = [[0] * (len(word) + 1) for i in range((len(word) + 1))]
+        li = self.getMatrix(li, word, sub)
+        j, subLen = self.getLenLongestSubstring(li)
+        return self.getLongestSubstring(subLen, j, sub)
 
+    def getMatrix(self, li, word, sub):
+        i = 1
+        for w in word:
+            j = 1
+            for s in sub:
+                if w == s:
+                    li[i][j] = (li[i - 1][j - 1]) + 1
+                else:
+                    li[i][j] = 0
+                j += 1
+            i += 1
+        return li
+
+    def getLenLongestSubstring(self, li):
+        subLen = 0
+        i = 1
+        for lis in li[1:]:
+            maxVal = max(lis)
+            if subLen <= maxVal:
+                subLen = maxVal
+                j = lis.index(maxVal)
+            i += 1
+        return j, subLen
+
+    def getLongestSubstring(self, subLen, j, sub):
+        substr = sub[j - 1]
+        i = 0
+        while i < subLen - 1:
+            j -= 1
+            substr += sub[j - 1]
+            i += 1
+        return substr
